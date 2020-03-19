9a88d3f9edb8013d487cf3fa6282d882f2ccc511
MoCuishle28 <20164706@s.hlju.edu.cn>
2020-03-20 02:10:38 +0800

    use cpu

--- experiment/combineDDPG/myModel.py
+++ experiment/combineDDPG/myModel.py
@@ -490,5 +490,6 @@ if __name__ == '__main__':
 	parser.add_argument('--dropout', type=float, default=0.0)	# dropout (BN 可以不需要)
 	args = parser.parse_args()
 
-	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	device = torch.device('cpu')
 	main(args, device)
