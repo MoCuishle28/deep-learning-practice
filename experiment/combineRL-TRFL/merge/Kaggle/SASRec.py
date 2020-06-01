48208ca2d871a73d5610844e3e8d70ab72a03c31
MoCuishle28 <20164706@s.hlju.edu.cn>
2020-06-01 15:18:59 +0800

    baseline

--- experiment/combineRL-TRFL/merge/Kaggle/SASRec.py
+++ experiment/combineRL-TRFL/merge/Kaggle/SASRec.py
@@ -9,9 +9,11 @@ import os
 import argparse
 import trfl
 from utils import *
+from SASRecModules import *
 
 def parse_args():
-	parser = argparse.ArgumentParser(description="Run supervised Caser.")
+	parser = argparse.ArgumentParser(description="Run supervised SASRec.")
+
 	parser.add_argument('--v', default='v')
 	parser.add_argument('--base_log_dir', default='baseline-log/')
 	parser.add_argument('--topk', default='5,10,20')
@@ -29,90 +31,64 @@ def parse_args():
 
 	parser.add_argument('--lr', type=float, default=0.005,
 						help='Learning rate.')
-	parser.add_argument('--num_filters', type=int, default=16,
-						help='Number of filters per filter size (default: 128)')
-	parser.add_argument('--filter_sizes', nargs='?', default='[2,3,4]',
-						help='Specify the filter_size')
+
+	parser.add_argument('--num_heads', default=1, type=int)
+	parser.add_argument('--num_blocks', default=1, type=int)
 	parser.add_argument('--dropout_rate', default=0.1, type=float)
+
 	parser.add_argument('--mem_ratio', type=float, default=0.2)
 	return parser.parse_args()
 
 
-class Caser:
-	def __init__(self, hidden_size, learning_rate, item_num, state_size):
+class SASRecnetwork:
+	def __init__(self, hidden_size,learning_rate,item_num,state_size):
 		self.state_size = state_size
 		self.hw = state_size
 		self.learning_rate = learning_rate
 		self.hidden_size = hidden_size
 		self.item_num = int(item_num)
 		self.is_training = tf.placeholder(tf.bool, shape=())
-		all_embeddings = self.initialize_embeddings()
+
+		all_embeddings=self.initialize_embeddings()
 
 		self.inputs = tf.placeholder(tf.int32, [None, state_size],name='inputs')
 		self.len_state=tf.placeholder(tf.int32, [None],name='len_state')
-		self.target= tf.placeholder(tf.int32, [None],name='target') # target item, to calculate ce loss
-
-		mask = tf.expand_dims(tf.to_float(tf.not_equal(self.inputs, item_num)), -1)
+		self.target = tf.placeholder(tf.int32, [None],name='target') # target item, to calculate ce loss
 
 		self.input_emb=tf.nn.embedding_lookup(all_embeddings['state_embeddings'],self.inputs)
-		self.input_emb*=mask
-		self.embedded_chars_expanded = tf.expand_dims(self.input_emb, -1)
+		# Positional Encoding
+		pos_emb=tf.nn.embedding_lookup(all_embeddings['pos_embeddings'],tf.tile(tf.expand_dims(tf.range(tf.shape(self.inputs)[1]), 0), [tf.shape(self.inputs)[0], 1]))
+		self.seq=self.input_emb+pos_emb
 
-		# Create a convolution + maxpool layer for each filter size
-		pooled_outputs = []
-		num_filters=args.num_filters
-		filter_sizes=eval(args.filter_sizes)
-		for i, filter_size in enumerate(filter_sizes):
-			with tf.name_scope("conv-maxpool-%s" % filter_size):
-				# Convolution Layer
-				filter_shape = [filter_size, self.hidden_size, 1, num_filters]
-				W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-				b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-
-				conv = tf.nn.conv2d(
-					self.embedded_chars_expanded,
-					W,
-					strides=[1, 1, 1, 1],
-					padding="VALID",
-					name="conv")
-				# Apply nonlinearity
-				h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-				# Maxpooling over the outputs
-				# new shape after max_pool[?, 1, 1, num_filters]
-				# be carefyul, the  new_sequence_length has changed because of wholesession[:, 0:-1]
-				pooled = tf.nn.max_pool(
-					h,
-					ksize=[1, state_size - filter_size + 1, 1, 1],
-					strides=[1, 1, 1, 1],
-					padding='VALID',
-					name="pool")
-				pooled_outputs.append(pooled)
-
-		# Combine all the pooled features
-		num_filters_total = num_filters * len(filter_sizes)
-		self.h_pool = tf.concat(pooled_outputs, 3)
-		self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])  # shape=[batch_size, 384]
-		# design the veritcal cnn
-		with tf.name_scope("conv-verical"):
-			filter_shape = [self.state_size, 1, 1, 1]
-			W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-			b = tf.Variable(tf.constant(0.1, shape=[1]), name="b")
-			conv = tf.nn.conv2d(
-				self.embedded_chars_expanded,
-				W,
-				strides=[1, 1, 1, 1],
-				padding="VALID",
-				name="conv")
-			h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-		self.vcnn_flat = tf.reshape(h, [-1, self.hidden_size])
-		self.final = tf.concat([self.h_pool_flat, self.vcnn_flat], 1)  # shape=[batch_size, 384+100]
-
-		# Add dropout
-		with tf.name_scope("dropout"):
-			self.state_hidden = tf.layers.dropout(self.final,
+		mask = tf.expand_dims(tf.to_float(tf.not_equal(self.inputs, item_num)), -1)
+		#Dropout
+		self.seq = tf.layers.dropout(self.seq,
 									 rate=args.dropout_rate,
 									 training=tf.convert_to_tensor(self.is_training))
-		self.state_hidden=self.final    # shape=(?, 112)
+		self.seq *= mask
+
+		# Build blocks
+
+		for i in range(args.num_blocks):
+			with tf.variable_scope("num_blocks_%d" % i):
+				# Self-attention
+				self.seq = multihead_attention(queries=normalize(self.seq),
+											   keys=self.seq,
+											   num_units=self.hidden_size,
+											   num_heads=args.num_heads,
+											   dropout_rate=args.dropout_rate,
+											   is_training=self.is_training,
+											   causality=True,
+											   scope="self_attention")
+
+				# Feed forward
+				self.seq = feedforward(normalize(self.seq), num_units=[self.hidden_size, self.hidden_size],
+									   dropout_rate=args.dropout_rate,
+									   is_training=self.is_training)
+				self.seq *= mask
+
+		self.seq = normalize(self.seq)
+		self.state_hidden=extract_axis_1(self.seq, self.len_state - 1)
 
 		self.output = tf.contrib.layers.fully_connected(self.state_hidden,self.item_num,activation_fn=None,scope='fc')
 
@@ -120,13 +96,16 @@ class Caser:
 		self.loss = tf.reduce_mean(self.loss)
 		self.opt = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
-
 	def initialize_embeddings(self):
 		all_embeddings = dict()
 		state_embeddings= tf.Variable(tf.random_normal([self.item_num+1, self.hidden_size], 0.0, 0.01),
 			name='state_embeddings')
+		pos_embeddings=tf.Variable(tf.random_normal([self.state_size, self.hidden_size], 0.0, 0.01),
+			name='pos_embeddings')
 		all_embeddings['state_embeddings']=state_embeddings
+		all_embeddings['pos_embeddings']=pos_embeddings
 		return all_embeddings
+
 
 def init_log(args):
 	if not os.path.exists(args.base_log_dir):
@@ -153,14 +132,12 @@ if __name__ == '__main__':
 		os.path.join(data_directory, 'data_statis.df'))  # read data statistics, includeing state_size and item_num
 	state_size = data_statis['state_size'][0]  # the length of history to define the state
 	item_num = data_statis['item_num'][0]  # total number of items
-	# save_file = 'pretrain-GRU/%d' % (hidden_size)
 
 	tf.reset_default_graph()
 
-	CaserRec = Caser(hidden_size=args.hidden_factor, learning_rate=args.lr,item_num=item_num,state_size=state_size)
+	SASRec = SASRecnetwork(hidden_size=args.hidden_factor, learning_rate=args.lr,item_num=item_num,state_size=state_size)
 
 	replay_buffer = pd.read_pickle(os.path.join(data_directory, 'replay_buffer.df'))
-	# saver = tf.train.Saver()
 
 	total_step = 0
 	max_ndcg_and_epoch = [[0, 0] for _ in args.topk.split(',')]	# (ng_inter, step)
@@ -175,16 +152,20 @@ if __name__ == '__main__':
 			for j in range(num_batches):
 				batch = replay_buffer.sample(n=args.batch_size).to_dict()
 				state = list(batch['state'].values())
-				# len_state = list(batch['len_state'].values())
-				target=list(batch['action'].values())
-				loss, _ = sess.run([CaserRec.loss, CaserRec.opt],
-								   feed_dict={CaserRec.inputs: state,
-											  CaserRec.target: target,
-											  CaserRec.is_training:True})
+				len_state = list(batch['len_state'].values())
+				target = list(batch['action'].values())
+				# seq,seq_test=sess.run([SASRec.seq,SASRec.seq_test],
+				#                    feed_dict={SASRec.inputs: state,
+				#                               SASRec.len_state: len_state,
+				#                               SASRec.target: target,
+				#                               SASRec.is_training:True})
+				loss, _ = sess.run([SASRec.loss, SASRec.opt],
+								   feed_dict={SASRec.inputs: state,
+											  SASRec.len_state: len_state,
+											  SASRec.target: target,
+											  SASRec.is_training:True})
 				total_step += 1
-				if (total_step == 1) or (total_step % 200 == 0):
+				if total_step % 200 == 0:
 					print("the loss in %dth batch is: %f" % (total_step, loss))
-					logging.info("the loss in %dth batch is: %f" % (total_step, loss))
 				if total_step % args.eval_interval == 0:
-					evaluate(args, CaserRec, sess, max_ndcg_and_epoch, total_step, logging)
-		# saver.save(sess, save_file)
+					evaluate(args, SASRec, sess, max_ndcg_and_epoch, total_step, logging)
--- experiment/combineRL-TRFL/merge/RC15/GRU.py
+++ experiment/combineRL-TRFL/merge/RC15/GRU.py
@@ -11,7 +11,8 @@ import trfl
 from utils import *
 
 def parse_args():
-	parser = argparse.ArgumentParser(description="Run supervised Caser.")
+	parser = argparse.ArgumentParser(description="Run supervised GRU.")
+
 	parser.add_argument('--v', default='v')
 	parser.add_argument('--base_log_dir', default='baseline-log/')
 	parser.add_argument('--topk', default='5,10,20')
@@ -29,92 +30,35 @@ def parse_args():
 
 	parser.add_argument('--lr', type=float, default=0.01,
 						help='Learning rate.')
-	parser.add_argument('--num_filters', type=int, default=16,
-						help='Number of filters per filter size (default: 128)')
-	parser.add_argument('--filter_sizes', nargs='?', default='[2,3,4]',
-						help='Specify the filter_size')
-	parser.add_argument('--dropout_rate', default=0.1, type=float)
 	parser.add_argument('--mem_ratio', type=float, default=0.2)
 	return parser.parse_args()
 
 
-class Caser:
-	def __init__(self, hidden_size, learning_rate, item_num, state_size):
+class GRUnetwork:
+	def __init__(self, hidden_size,learning_rate,item_num,state_size):
 		self.state_size = state_size
-		self.hw = state_size
 		self.learning_rate = learning_rate
-		self.hidden_size = hidden_size
-		self.item_num = int(item_num)
+		self.hidden_size=hidden_size
+		self.item_num=int(item_num)
+		self.hw = state_size
 		self.is_training = tf.placeholder(tf.bool, shape=())
-		all_embeddings = self.initialize_embeddings()
+
+		all_embeddings=self.initialize_embeddings()
 
 		self.inputs = tf.placeholder(tf.int32, [None, state_size],name='inputs')
 		self.len_state=tf.placeholder(tf.int32, [None],name='len_state')
 		self.target= tf.placeholder(tf.int32, [None],name='target') # target item, to calculate ce loss
 
-		mask = tf.expand_dims(tf.to_float(tf.not_equal(self.inputs, item_num)), -1)
-
 		self.input_emb=tf.nn.embedding_lookup(all_embeddings['state_embeddings'],self.inputs)
-		self.input_emb*=mask
-		self.embedded_chars_expanded = tf.expand_dims(self.input_emb, -1)
 
-		# Create a convolution + maxpool layer for each filter size
-		pooled_outputs = []
-		num_filters=args.num_filters
-		filter_sizes=eval(args.filter_sizes)
-		for i, filter_size in enumerate(filter_sizes):
-			with tf.name_scope("conv-maxpool-%s" % filter_size):
-				# Convolution Layer
-				filter_shape = [filter_size, self.hidden_size, 1, num_filters]
-				W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-				b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+		gru_out, self.states_hidden= tf.nn.dynamic_rnn(
+			tf.contrib.rnn.GRUCell(self.hidden_size),
+			self.input_emb,
+			dtype=tf.float32,
+			sequence_length=self.len_state,
+		)
 
-				conv = tf.nn.conv2d(
-					self.embedded_chars_expanded,
-					W,
-					strides=[1, 1, 1, 1],
-					padding="VALID",
-					name="conv")
-				# Apply nonlinearity
-				h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-				# Maxpooling over the outputs
-				# new shape after max_pool[?, 1, 1, num_filters]
-				# be carefyul, the  new_sequence_length has changed because of wholesession[:, 0:-1]
-				pooled = tf.nn.max_pool(
-					h,
-					ksize=[1, state_size - filter_size + 1, 1, 1],
-					strides=[1, 1, 1, 1],
-					padding='VALID',
-					name="pool")
-				pooled_outputs.append(pooled)
-
-		# Combine all the pooled features
-		num_filters_total = num_filters * len(filter_sizes)
-		self.h_pool = tf.concat(pooled_outputs, 3)
-		self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])  # shape=[batch_size, 384]
-		# design the veritcal cnn
-		with tf.name_scope("conv-verical"):
-			filter_shape = [self.state_size, 1, 1, 1]
-			W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-			b = tf.Variable(tf.constant(0.1, shape=[1]), name="b")
-			conv = tf.nn.conv2d(
-				self.embedded_chars_expanded,
-				W,
-				strides=[1, 1, 1, 1],
-				padding="VALID",
-				name="conv")
-			h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-		self.vcnn_flat = tf.reshape(h, [-1, self.hidden_size])
-		self.final = tf.concat([self.h_pool_flat, self.vcnn_flat], 1)  # shape=[batch_size, 384+100]
-
-		# Add dropout
-		with tf.name_scope("dropout"):
-			self.state_hidden = tf.layers.dropout(self.final,
-									 rate=args.dropout_rate,
-									 training=tf.convert_to_tensor(self.is_training))
-		self.state_hidden=self.final    # shape=(?, 112)
-
-		self.output = tf.contrib.layers.fully_connected(self.state_hidden,self.item_num,activation_fn=None,scope='fc')
+		self.output = tf.contrib.layers.fully_connected(self.states_hidden,self.item_num,activation_fn=None,scope='fc')
 
 		self.loss=tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.target,logits=self.output)
 		self.loss = tf.reduce_mean(self.loss)
@@ -127,6 +71,7 @@ class Caser:
 			name='state_embeddings')
 		all_embeddings['state_embeddings']=state_embeddings
 		return all_embeddings
+
 
 def init_log(args):
 	if not os.path.exists(args.base_log_dir):
@@ -157,10 +102,9 @@ if __name__ == '__main__':
 
 	tf.reset_default_graph()
 
-	CaserRec = Caser(hidden_size=args.hidden_factor, learning_rate=args.lr,item_num=item_num,state_size=state_size)
+	GRUnet = GRUnetwork(hidden_size=args.hidden_factor, learning_rate=args.lr,item_num=item_num,state_size=state_size)
 
 	replay_buffer = pd.read_pickle(os.path.join(data_directory, 'replay_buffer.df'))
-	# saver = tf.train.Saver()
 
 	total_step = 0
 	max_ndcg_and_epoch = [[0, 0] for _ in args.topk.split(',')]	# (ng_inter, step)
@@ -175,16 +119,14 @@ if __name__ == '__main__':
 			for j in range(num_batches):
 				batch = replay_buffer.sample(n=args.batch_size).to_dict()
 				state = list(batch['state'].values())
-				# len_state = list(batch['len_state'].values())
-				target=list(batch['action'].values())
-				loss, _ = sess.run([CaserRec.loss, CaserRec.opt],
-								   feed_dict={CaserRec.inputs: state,
-											  CaserRec.target: target,
-											  CaserRec.is_training:True})
+				len_state = list(batch['len_state'].values())
+				target = list(batch['action'].values())
+				loss, _ = sess.run([GRUnet.loss, GRUnet.opt],
+								   feed_dict={GRUnet.inputs: state,
+											  GRUnet.len_state: len_state,
+											  GRUnet.target: target})
 				total_step += 1
-				if (total_step == 1) or (total_step % 200 == 0):
+				if total_step % 200 == 0:
 					print("the loss in %dth batch is: %f" % (total_step, loss))
-					logging.info("the loss in %dth batch is: %f" % (total_step, loss))
 				if total_step % args.eval_interval == 0:
-					evaluate(args, CaserRec, sess, max_ndcg_and_epoch, total_step, logging)
-		# saver.save(sess, save_file)
+					evaluate(args, GRUnet, sess, max_ndcg_and_epoch, total_step, logging)
