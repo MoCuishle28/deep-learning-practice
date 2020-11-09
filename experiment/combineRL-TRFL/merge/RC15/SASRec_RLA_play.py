cd62e1e5c0f6561ea6cc566c6ace88dd08e82bd9
Ubuntu <zhanggaoming028@gmail.com>
2020-11-09 23:00:43 +0800

    pre-train SASRec

--- experiment/combineRL-TRFL/merge/RC15/SASRec_RLA_play.py
+++ experiment/combineRL-TRFL/merge/RC15/SASRec_RLA_play.py
@@ -98,15 +98,21 @@ class Agent:
 			self.action_size = int(self.state_hidden.shape[-1])
 
 			# DDPG
-			self.actor_output = tf.contrib.layers.fully_connected(tf.stop_gradient(self.state_hidden), self.action_size, 
-					activation_fn=tf.nn.tanh, 
-					weights_regularizer=tf.contrib.layers.l2_regularizer(args.weight_decay))
+			actor = eval(args.actor_layers)
+			actor.append(self.action_size)
+			with tf.variable_scope("actor"):
+				self.actor_output = mlp(tf.stop_gradient(self.state_hidden), self.is_training, hidden_sizes=actor, 
+					dropout_rate=args.atten_dropout_rate, 
+					l2=tf.contrib.layers.l2_regularizer(args.weight_decay))
 			self.actor_out_ = self.actor_output * max_action
 
 			self.critic_input = tf.concat([self.actor_out_, tf.stop_gradient(self.state_hidden)], axis=1)
-			self.critic_output = tf.contrib.layers.fully_connected(self.critic_input, 1, 
-				activation_fn=None, 
-				weights_regularizer=tf.contrib.layers.l2_regularizer(args.weight_decay))
+			critic = eval(args.critic_layers)
+			critic.append(1)
+			with tf.variable_scope("critic"):
+				self.critic_output = mlp(self.critic_input, self.is_training, hidden_sizes=critic, 
+					output_activation=None, dropout_rate=args.atten_dropout_rate, 
+					l2=tf.contrib.layers.l2_regularizer(args.weight_decay))
 
 			self.dpg_return = trfl.dpg(self.critic_output, self.actor_out_, 
 				dqda_clipping=dqda_clipping, clip_norm=clip_norm)
@@ -122,13 +128,24 @@ class Agent:
 			# SASRec
 			self.ranking_model_input = self.actor_out_ + self.state_hidden
 
-			self.logits = tf.contrib.layers.fully_connected(self.ranking_model_input, self.item_num,
-				activation_fn=None, weights_regularizer=tf.contrib.layers.l2_regularizer(args.weight_decay))
+			with tf.variable_scope('output_layer'):
+				self.logits = tf.contrib.layers.fully_connected(self.ranking_model_input, self.item_num,
+					activation_fn=None, weights_regularizer=tf.contrib.layers.l2_regularizer(args.weight_decay))
+			# pre-train
+			with tf.variable_scope('output_layer', reuse=True):
+				self.output = tf.contrib.layers.fully_connected(self.state_hidden, self.item_num,
+					activation_fn=None, weights_regularizer=tf.contrib.layers.l2_regularizer(args.weight_decay))
 
 			self.ce_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.target_items,
 				logits=self.logits)
 			self.ranking_model_loss = tf.reduce_mean(self.ce_loss)
 			self.model_optim = tf.train.AdamOptimizer(args.mlr).minimize(self.ranking_model_loss)
+
+			# pre-train
+			self.ce_loss2 = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.target_items,
+				logits=self.output)
+			self.pre_train_loss = tf.reduce_mean(self.ce_loss2)
+			self.pre_train_opt = tf.train.AdamOptimizer(args.mlr).minimize(self.pre_train_loss)
 
 
 	def initialize_embeddings(self):
@@ -159,6 +176,7 @@ class Run(object):
 		self.topk = [int(x) for x in args.topk.split(',')]
 		self.replay_buffer = pd.read_pickle(os.path.join(args.base_data_dir, 'replay_buffer.df'))
 		self.memory = ReplayMemory(args.maxlen)
+		self.sess = None
 
 	def sample_data(self):
 		batch = self.replay_buffer.sample(n=args.batch_size).to_dict()
@@ -184,6 +202,45 @@ class Run(object):
 			rewards.append(ndcg)
 		return rewards
 
+	def pre_train(self):
+		num_rows = self.replay_buffer.shape[0]
+		num_batches = int(num_rows / self.args.batch_size)
+		max_ndcg_and_epoch = [[0, 0] for _ in args.topk.split(',')]	# (ng_inter, step)
+		total_step = 0
+		fix_rec = False
+
+		for i_epoch in range(self.args.epoch):
+			for j in range(num_batches):
+				ui_seq, ui_seq_len, _, _, target_items, _ = self.sample_data()
+				ranking_model_loss, _ = self.sess.run([
+					self.main_agent.pre_train_loss, 
+					self.main_agent.pre_train_opt], 
+					feed_dict={
+					self.main_agent.inputs: ui_seq, 
+					self.main_agent.len_state: ui_seq_len,
+					self.main_agent.target_items: target_items,
+					self.main_agent.is_training: True})
+
+				total_step += 1
+				if (total_step == 1) or (total_step % 200 == 0):
+					ranking_model_loss = round(ranking_model_loss.item(), 5)
+					info = f"Pre-train epoch:{i_epoch} Step:{total_step}, ranking model loss:{ranking_model_loss}"
+					print(info)
+					logging.info(info)
+				if (total_step >= self.args.start_eval) and (total_step % self.args.eval_interval == 0):
+					t1 = time.time()
+					evaluate(self.args, self.main_agent, self.sess, max_ndcg_and_epoch, total_step, logging)
+					t2 = time.time()
+					print(f'Time:{t2 - t1}')
+					logging.info(f'Time:{t2 - t1}')
+					if (total_step >= self.args.start_eval) and (total_step - max_ndcg_and_epoch[0][1] >= 6000) and (total_step - max_ndcg_and_epoch[1][1] >= 6000) and (total_step - max_ndcg_and_epoch[2][1] >= 6000):
+						fix_rec = True
+						break
+			if fix_rec:
+				print('END Pre-train')
+				logging.info('END Pre-train')
+				break
+
 	def train(self):
 		num_rows = self.replay_buffer.shape[0]
 		num_batches = int(num_rows / self.args.batch_size)
@@ -191,116 +248,112 @@ class Run(object):
 		total_step = 0
 		fix_rec = False
 
-		gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=self.args.mem_ratio)
-		with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-			sess.run(tf.global_variables_initializer())
-			sess.graph.finalize()
-			sess.run(self.copy_weight)		# copy weights
-			discount = [self.args.gamma] * self.args.batch_size
-			for i_epoch in range(self.args.epoch):
-				for j in range(num_batches):
-					ui_seq, ui_seq_len, _, _, target_items, _ = self.sample_data()
-					state = sess.run(self.main_agent.state_hidden, feed_dict={
-						self.main_agent.inputs: ui_seq,
-						self.main_agent.len_state: ui_seq_len,
-						self.main_agent.is_training: False
-						})
+		self.sess.run(self.copy_weight)		# copy weights
+		discount = [self.args.gamma] * self.args.batch_size
+		for i_epoch in range(self.args.epoch):
+			for j in range(num_batches):
+				ui_seq, ui_seq_len, _, _, target_items, _ = self.sample_data()
+				state = self.sess.run(self.main_agent.state_hidden, feed_dict={
+					self.main_agent.inputs: ui_seq,
+					self.main_agent.len_state: ui_seq_len,
+					self.main_agent.is_training: False
+					})
 
-					interaction_time = 0
-					while True:
-						actions = sess.run(self.main_agent.actor_out_, feed_dict={
-							self.main_agent.state_hidden: state,
-							self.main_agent.is_training: False})
-						# add noise
-						noise = np.random.normal(0, self.args.noise_var, size=self.main_agent.action_size).clip(-self.args.noise_clip, self.args.noise_clip)
-						actions = (actions + noise).clip(-self.args.max_action, self.args.max_action)
+				interaction_time = 0
+				while True:
+					actions = self.sess.run(self.main_agent.actor_out_, feed_dict={
+						self.main_agent.state_hidden: state,
+						self.main_agent.is_training: False})
+					# add noise
+					noise = np.random.normal(0, self.args.noise_var, size=self.main_agent.action_size).clip(-self.args.noise_clip, self.args.noise_clip)
+					actions = (actions + noise).clip(-self.args.max_action, self.args.max_action)
 
-						if (interaction_time == 0) and (not fix_rec):
-							ce_loss, ranking_model_loss, _ = sess.run([
-								self.main_agent.ce_loss,
-								self.main_agent.ranking_model_loss, 
-								self.main_agent.model_optim], 
-								feed_dict={
-								self.main_agent.inputs: ui_seq, 
-								self.main_agent.len_state: ui_seq_len,
-								self.main_agent.actor_out_: actions,
-								self.main_agent.target_items: target_items,
-								self.main_agent.is_training: True})
-						# get new state
-						state_new = sess.run(self.main_agent.ranking_model_input, 
+					if (interaction_time == 0) and (not fix_rec	):
+						ce_loss, ranking_model_loss, _ = self.sess.run([
+							self.main_agent.ce_loss,
+							self.main_agent.ranking_model_loss, 
+							self.main_agent.model_optim], 
 							feed_dict={
-							self.main_agent.state_hidden: state, 
+							self.main_agent.inputs: ui_seq, 
+							self.main_agent.len_state: ui_seq_len,
 							self.main_agent.actor_out_: actions,
-							self.main_agent.is_training: False})
+							self.main_agent.target_items: target_items,
+							self.main_agent.is_training: True})
+					# get new state
+					state_new = self.sess.run(self.main_agent.ranking_model_input, 
+						feed_dict={
+						self.main_agent.state_hidden: state, 
+						self.main_agent.actor_out_: actions,
+						self.main_agent.is_training: False})
 
-						# target logits
-						logits = sess.run(self.target_agent.logits,
-							feed_dict={
-							self.target_agent.state_hidden: state, 
-							self.target_agent.actor_out_: actions,
+					# target logits
+					logits = self.sess.run(self.target_agent.logits,
+						feed_dict={
+						self.target_agent.state_hidden: state, 
+						self.target_agent.actor_out_: actions,
+						self.target_agent.is_training: False})
+					rewards = self.cal_rewards(logits, target_items)	# get rewards
+
+					state_new_list = []		# continue play
+					for idx, (s, a, s_, r) in enumerate(zip(state, actions, state_new, rewards)):
+						if r == 0: 		# continue play
+							state_new_list.append(s_)
+						done = True if r != 0 else False
+						self.memory.push(s, a, s_, r, done)
+
+					if interaction_time % self.args.train_interval == 0:
+						transitions = self.memory.sample(self.args.batch_size)
+						batch = Transition(*zip(*transitions))
+						state, actions = batch.state, batch.action
+						rewards, next_state, is_done = batch.reward, batch.next_state, batch.is_done
+
+						target_v = self.sess.run(self.target_agent.critic_output, feed_dict={
+							self.target_agent.state_hidden: next_state,
 							self.target_agent.is_training: False})
-						rewards = self.cal_rewards(logits, target_items)	# get rewards
+						target_v = target_v.squeeze()
+						for index in range(self.args.batch_size):
+							if is_done[index]:
+								target_v[index] = 0.0
 
-						state_new_list = []		# continue play
-						for idx, (s, a, s_, r) in enumerate(zip(state, actions, state_new, rewards)):
-							if r == 0: 		# continue play
-								state_new_list.append(s_)
-							done = True if r != 0 else False
-							self.memory.push(s, a, s_, r, done)
-
-						if interaction_time % self.args.train_interval == 0:
-							transitions = self.memory.sample(self.args.batch_size)
-							batch = Transition(*zip(*transitions))
-							state, actions = batch.state, batch.action
-							rewards, next_state, is_done = batch.reward, batch.next_state, batch.is_done
-
-							target_v = sess.run(self.target_agent.critic_output, feed_dict={
-								self.target_agent.state_hidden: next_state,
-								self.target_agent.is_training: False})
-							target_v = target_v.squeeze()
-							for index in range(self.args.batch_size):
-								if is_done[index]:
-									target_v[index] = 0.0
-
-							critic_loss, _ = sess.run([self.main_agent.critic_loss, 
-								self.main_agent.critic_optim], 
-								feed_dict={self.main_agent.state_hidden:state, 
-								self.main_agent.actor_out_: actions, 
-								self.main_agent.reward: rewards,
-								self.main_agent.discount: discount,
-								self.main_agent.target: target_v,
-								self.main_agent.is_training: True})
-							actor_loss, _ = sess.run([self.main_agent.actor_loss, 
-								self.main_agent.actor_optim],
-								feed_dict={self.main_agent.state_hidden: state, 
-								self.main_agent.is_training: True})
-							sess.run(self.target_network_update_ops)		# update target net
-							info = f'Step:[{total_step}] train agent, batch size:{len(state_new_list)} V:{self.args.v}'
-							print(info)
-							# logging.info(info)
-						interaction_time += 1
-						state = state_new_list		# next state
-						if (state_new_list == []) or (interaction_time >= self.args.max_interaction):
-							print('Interaction END!')
-							break
-
-					total_step += 1
-					if (total_step == 1) or (total_step % 200 == 0):
-						if not fix_rec:
-							ranking_model_loss = round(ranking_model_loss.item(), 5)
-						actor_loss, critic_loss = round(actor_loss.item(), 5), round(critic_loss.item(), 5)
-						info = f"epoch:{i_epoch} Step:{total_step}, ranking model loss:{ranking_model_loss}, actor loss:{actor_loss}, critic loss:{critic_loss}"
+						critic_loss, _ = self.sess.run([self.main_agent.critic_loss, 
+							self.main_agent.critic_optim], 
+							feed_dict={self.main_agent.state_hidden:state, 
+							self.main_agent.actor_out_: actions, 
+							self.main_agent.reward: rewards,
+							self.main_agent.discount: discount,
+							self.main_agent.target: target_v,
+							self.main_agent.is_training: True})
+						actor_loss, _ = self.sess.run([self.main_agent.actor_loss, 
+							self.main_agent.actor_optim],
+							feed_dict={self.main_agent.state_hidden: state, 
+							self.main_agent.is_training: True})
+						self.sess.run(self.target_network_update_ops)		# update target net
+						info = f'Step:[{total_step}] train agent, batch size:{len(state_new_list)} V:{self.args.v}'
 						print(info)
-						logging.info(info)
-					if (total_step >= self.args.start_eval) and (total_step % self.args.eval_interval == 0):
-						t1 = time.time()
-						evaluate_multi_head(self.args, self.main_agent, sess, max_ndcg_and_epoch, total_step, logging)
-						# evaluate_with_actions(self.args, self.main_agent, sess, max_ndcg_and_epoch, total_step, logging)
-						t2 = time.time()
-						print(f'Time:{t2 - t1}')
-						logging.info(f'Time:{t2 - t1}')
-						if (total_step >= self.args.start_eval) and (total_step - max_ndcg_and_epoch[0][1] >= 6000) and (total_step - max_ndcg_and_epoch[1][1] >= 6000) and (total_step - max_ndcg_and_epoch[2][1] >= 6000):
-							fix_rec = True
+						# logging.info(info)
+					interaction_time += 1
+					state = state_new_list		# next state
+					if (state_new_list == []) or (interaction_time >= self.args.max_interaction):
+						print('Interaction END!')
+						break
+
+				total_step += 1
+				if (total_step == 1) or (total_step % 200 == 0):
+					if not fix_rec:
+						ranking_model_loss = round(ranking_model_loss.item(), 5)
+					actor_loss, critic_loss = round(actor_loss.item(), 5), round(critic_loss.item(), 5)
+					info = f"epoch:{i_epoch} Step:{total_step}, ranking model loss:{ranking_model_loss}, actor loss:{actor_loss}, critic loss:{critic_loss}"
+					print(info)
+					logging.info(info)
+				if (total_step == 1) or ((total_step >= self.args.start_eval) and (total_step % self.args.eval_interval == 0)):
+					t1 = time.time()
+					evaluate_multi_head(self.args, self.main_agent, self.sess, max_ndcg_and_epoch, total_step, logging)
+					# evaluate_with_actions(self.args, self.main_agent, self.sess, max_ndcg_and_epoch, total_step, logging)
+					t2 = time.time()
+					print(f'Time:{t2 - t1}')
+					logging.info(f'Time:{t2 - t1}')
+					if (total_step >= self.args.start_eval) and (total_step - max_ndcg_and_epoch[0][1] >= 6000) and (total_step - max_ndcg_and_epoch[1][1] >= 6000) and (total_step - max_ndcg_and_epoch[2][1] >= 6000):
+						fix_rec = True
 
 
 def main(args):
@@ -309,7 +362,13 @@ def main(args):
 	target_agent = Agent(args, name='target', max_action=args.max_action)
 
 	run = Run(args, main_agent, target_agent)
-	run.train()
+	gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.mem_ratio)
+	with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+		sess.run(tf.global_variables_initializer())
+		sess.graph.finalize()
+		run.sess = sess
+		run.pre_train()
+		run.train()
 
 
 def init_log(args):
@@ -337,7 +396,7 @@ if __name__ == '__main__':
 	parser.add_argument('--base_data_dir', default=base_data_dir + 'RC15')
 	parser.add_argument('--topk', default='5,10,20')
 
-	parser.add_argument('--epoch', type=int, default=100)
+	parser.add_argument('--epoch', type=int, default=500)
 	parser.add_argument('--eval_interval', type=int, default=2000)
 	parser.add_argument('--start_eval', type=int, default=2000)
 	parser.add_argument('--eval_batch', type=int, default=10)
@@ -370,6 +429,10 @@ if __name__ == '__main__':
 	parser.add_argument('--maxlen', type=int, default=10000)
 	parser.add_argument('--max_interaction', type=int, default=30)
 	parser.add_argument('--train_interval', type=int, default=10)
+
+	parser.add_argument('--atten_dropout_rate', type=float, default=0.5)
+	parser.add_argument('--actor_layers', default="[]")
+	parser.add_argument('--critic_layers', default="[]")
 	args = parser.parse_args()
 
 	os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda
